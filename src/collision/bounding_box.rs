d95c28bd35550f2515f1cab42e10235fdf49cbd5
acerne <a.cerne89@gmail.com>
2020-12-17 11:22:10 +0100

    Added box-box collision detection and testing example

--- src/collision/bounding_box.rs
+++ src/collision/bounding_box.rs
@@ -124,7 +124,38 @@ impl BoundingBox {
             Some(Hit::new(
                 Point::new(point.x, self.center.y + self.half.h * dy.signum()),
                 Vector::new(0.0, dy.signum()),
-                Vector::new(0.0, dy * dy.signum()),
+                Vector::new(0.0, py * dy.signum()),
+            ))
+        }
+    }
+    pub fn hit_bounding_box(&self, bounding_box: BoundingBox) -> Option<Hit> {
+        let dx = bounding_box.center.x - self.center.x;
+        let px = (bounding_box.half.w + self.half.w) - dx.abs();
+        if px <= 0.0 {
+            return None;
+        }
+        let dy = bounding_box.center.y - self.center.y;
+        let py = (bounding_box.half.h + self.half.h) - dy.abs();
+        if py <= 0.0 {
+            return None;
+        }
+        if px < py {
+            Some(Hit::new(
+                Point::new(
+                    self.center.x + self.half.w * dx.signum(),
+                    bounding_box.center.y,
+                ),
+                Vector::new(dx.signum(), 0.0),
+                Vector::new(px * dx.signum(), 0.0),
+            ))
+        } else {
+            Some(Hit::new(
+                Point::new(
+                    bounding_box.center.x,
+                    self.center.y + self.half.h * dy.signum(),
+                ),
+                Vector::new(0.0, dy.signum()),
+                Vector::new(0.0, py * dy.signum()),
             ))
         }
     }
